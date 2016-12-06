4a5e095f48ff34d3ef32c6e152664d4ccbb2a15e
LiadW <liadwg2@gmail.com>
2016-12-06 19:16:04 +0200

    asdasdasd

--- base.py
+++ base.py
@@ -38,6 +38,7 @@ def get_host_graphlet_dict_summ(cap_file):
     return graphlet_dict
 
 
+
 # feature name map:
 # n - number of nodes
 # o - number of one degree nodes
@@ -45,46 +46,64 @@ def get_host_graphlet_dict_summ(cap_file):
 # alpha - max degree
 # beta - back degree of alpha
 def get_graphlet_features(graphlet):
-    sub_g_a = graphlet
-    sub_g_b = graphlet
+    sub_g_a = [graphlet]
+    sub_g_b = []
     result = {}
     lvl = 1
-    result["n%d" % lvl] = len(sub_g_a.keys())
-    result["o%d_%d" % (lvl, lvl+1)] = 0
-    result["alpha%d_%d" % (lvl, lvl+1)] = 0
-    tot = 0
-    alpha = None
-    for node in sub_g_a.keys():
-        tot += len(sub_g_a[node].keys())
-        if len(sub_g_a[node].keys()) == 1:
-            result["o%d_%d" % (lvl, lvl+1)] += 1
-        if len(sub_g_a[node].keys()) > result["alpha%d_%d" % (lvl, lvl+1)]:
-            result["alpha%d_%d" % (lvl, lvl+1)] = len(sub_g_a[node].keys())
-            alpha = node
-    result["myu%d_%d" % (lvl, lvl+1)] = tot/len(sub_g_a.keys())
+    while lvl <= 5:
+        if lvl < 5:
+            result["n%d" % lvl] = len(sub_g_a)
+            result["o%d_%d" % (lvl, lvl+1)] = 0
+            result["alpha%d_%d" % (lvl, lvl+1)] = 0
+            tot = 0
+            alpha = None
+            for node in sub_g_a:
+                tot += len(node.keys())
+                if len(node.keys()) == 1:
+                    result["o%d_%d" % (lvl, lvl+1)] += 1
+                if len(node.keys()) > result["alpha%d_%d" % (lvl, lvl+1)]:
+                    result["alpha%d_%d" % (lvl, lvl+1)] = len(node.keys())
+                    alpha = node
+            result["myu%d_%d" % (lvl, lvl+1)] = tot/len(sub_g_a)
 
-    if lvl > 1:
-        result["o%d_%d" % (lvl, lvl-1)] = 0
-        result["alpha%d_%d" % (lvl, lvl-1)] = 0
-        tot = 0
-        for node in sub_g_a.keys():
-            cnt = 0
-            for bnode in sub_g_b.keys():
-                if node in sub_g_b[bnode]:
-                    cnt += 1
-            tot += cnt
-            if cnt == 1:
-                result["o%d_%d" % (lvl, lvl-1)] += 1
-            if cnt > result["alpha%d_%d" % (lvl, lvl-1)]:
-                result["alpha%d_%d" % (lvl, lvl-1)] = cnt
-                if lvl < 5:
-                    pass
-            if node == alpha:
-                result["beta%d_%d" % (lvl, lvl+1)] = cnt
+        if lvl > 1:
+            result["o%d_%d" % (lvl, lvl-1)] = 0
+            result["alpha%d_%d" % (lvl, lvl-1)] = 0
+            tot = 0
+            for node in sub_g_a:
+                cnt = 0
+                for bnode in sub_g_b:
+                    if node in bnode.keys():
+                        cnt += 1
+                tot += cnt
+                if cnt == 1:
+                    result["o%d_%d" % (lvl, lvl-1)] += 1
+                if cnt > result["alpha%d_%d" % (lvl, lvl-1)]:
+                    result["alpha%d_%d" % (lvl, lvl-1)] = cnt
+                    if lvl < 5:
+                        result["beta%d_%d" % (lvl, lvl-1)] = len(node.keys())
+                if lvl < 5 and node == alpha:
+                    result["beta%d_%d" % (lvl, lvl+1)] = cnt
 
-        result["myu%d_%d" % (lvl, lvl-1)] = tot/len(sub_g_a.keys())
+            result["myu%d_%d" % (lvl, lvl-1)] = tot/len(sub_g_a)
+
+        temp = sub_g_a
+        if lvl <= 4:
+            sub_g_a = [key.values() for key in sub_g_a]
+        if lvl == 4:
+            last = []
+            for item in sub_g_a:
+                last.extend(item)
+            sub_g_a = last
+        sub_g_b = temp
+        lvl += 1
+
+    return result
 
 
 
 capture_summaries = pyshark.FileCapture("Test28_Id1_Stream1_100.pcap", only_summaries=True, display_filter="tcp.flags.syn == 1 and tcp.flags.ack == 1")
-g_dict = get_host_graphlet_dict_summ(capture_summaries)
+graphlet_dict = get_host_graphlet_dict_summ(capture_summaries)
+feature_dict = {}
+for graphlet_host in graphlet_dict:
+    feature_dict[graphlet_host] = get_graphlet_features(graphlet_dict[graphlet_host])
